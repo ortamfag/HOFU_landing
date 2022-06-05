98aa58979b905d3020640d036ea7b1db75c6c30f
ortamfag <a_zhidov@outlook.com>
2022-06-05 19:11:43 +0300

    FIX: footer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,6 @@
 const fixed = document.querySelector('#fixed')
 
 function opacity() {
-
-  if (window.innerWidth >= 590) {
   
     if (window.pageYOffset > 200) {
       fixed.classList.add('active-f')
@@ -13,18 +11,6 @@ function opacity() {
     }
 
   }
-
-  else
-    if (window.pageYOffset > 8000) {
-      fixed.classList.remove('active-f')
-    }
-
-    else{
-      fixed.classList.add('active-f')
-    }
-
-  
-}
 
 window.onscroll=opacity
 opacity()
