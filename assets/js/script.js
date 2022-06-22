d65fe72f630fbb20c6b657e1bac737ca32c796bf
ortamfag <a_zhidov@outlook.com>
2022-06-22 17:01:52 +0300

    ADD: scroll animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,6 +15,22 @@ function opacity() {
 window.onscroll=opacity
 opacity()
 
+
+//scroll
+function onEntry(entry) {
+  entry.forEach(change => {
+    if (change.isIntersecting) {
+      change.target.classList.add('element-show');
+    }
+  });
+}
+let options = { threshold: [0.5] };
+let observer = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.element-animation');
+for (let elm of elements) {
+  observer.observe(elm);
+}
+
 //slider
 let sliderItem = document.querySelectorAll('.problem__item')
 let sliderText = document.querySelectorAll(`.slider-text`)
